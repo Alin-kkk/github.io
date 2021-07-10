7c50937c259a3810012728910a1af81a7aac13aa
GitHub Action <action@github.com>
2021-07-10 03:06:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/alinkk/githubio/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alinkk/githubio/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alinkk.githubio.services
 
+import com.github.alinkk.githubio.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
